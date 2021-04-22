c5f0e405bb71a41d9fedb633105fac61717d6570
Igor <kuja.qja96@gmail.com>
2021-04-22 20:35:15 +0200

    Initial commit

---
+++ src/test/java/com/recordshop/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recordshop.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
